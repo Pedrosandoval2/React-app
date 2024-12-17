d39195362222f5e146e0d71c56d6d8f303870d19
Pedro <soporte@peruviandigital.com>
2024-12-17 15:21:53 -0500

    add child routes with suspense

--- src/routers/routers.ts
+++ src/routers/routers.ts
@@ -1,4 +1,5 @@
 import { lazy, LazyExoticComponent } from "react";
+import { NoLazy } from "../01-lazyload/pages/NoLazy";
 
 type JSXComponent = () => JSX.Element
 
@@ -16,27 +17,39 @@ export const route = {
     lazy3: 'lazy3'
 }
 
-const Lazy1 = lazy(() => import('../01-lazyload/pages/LazyPage1'))
+const LazyLayout = lazy(() => import('../01-lazyload/layout/LazyLayout'))
 const Lazy2 = lazy(() => import('../01-lazyload/pages/LazyPage2'))
 const Lazy3 = lazy(() => import('../01-lazyload/pages/LazyPage3'))
 
 export const ROUTES: Route[] = [
     {
-        to: `/${route.lazy1}`,
-        path: `${route.lazy1}`,
-        Component: Lazy1,
-        name: 'lazy-1'
+        path: `/lazyload/*`,
+        to: `/lazyload/`,
+        Component: LazyLayout,
+        name: 'lazyLayout'
     },
+    // {
+    //     to: `/${route.lazy1}`,
+    //     path: `${route.lazy1}`,
+    //     Component: LazyLayout,
+    //     name: 'lazy-1'
+    // },
     {
-        to: `/${route.lazy2}`,
-        path: `${route.lazy2}`,
-        Component: Lazy2,
-        name: 'lazy-2'
+        to: `/no-lazy`,
+        path: `no-lazy`,
+        Component: NoLazy,
+        name: 'No Lazy'
     },
-    {
-        to: `/${route.lazy3}`,
-        path: `${route.lazy3}`,
-        Component: Lazy3,
-        name: 'lazy-3'
-    }
+    // {
+    //     to: `/${route.lazy2}`,
+    //     path: `${route.lazy2}`,
+    //     Component: Lazy2,
+    //     name: 'lazy-2'
+    // },
+    // {
+    //     to: `/${route.lazy3}`,
+    //     path: `${route.lazy3}`,
+    //     Component: Lazy3,
+    //     name: 'lazy-3'
+    // }
 ]
